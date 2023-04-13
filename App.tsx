fea4f95ddb591dfda12e65c605922fe5101eb1f8
Naomi Plasterer <naomi@xmtp.com>
2023-04-13 09:48:34 -0700

    take a pass on sending a gm message

--- App.tsx
+++ App.tsx
@@ -1,16 +1,19 @@
 import React, {useRef, useState} from 'react';
 import {SafeAreaView, Text, Button} from 'react-native';
 import {WebView} from 'react-native-webview';
-import html from './xmtp/index.html';
+import { Wallet } from 'ethers';
+import { utils as SecpUtils } from '@noble/secp256k1';
 
 function App(): JSX.Element {
   const webView = useRef<WebView | null>(null);
   const [result, setResult] = useState('hi pat');
 
-  function onPress() {
-    console.log('on press called');
+  function sendGM() {
+    console.log('on send gm');
+    const account = new Wallet(SecpUtils.randomPrivateKey());
+    const recipient = "0x33FA52E6a9DBFca57ed277491DBD8Ba5A0B248f4"
     webView.current?.injectJavaScript(`
-      window.add(1)(2)
+      window.sendGM(account, recipient)
 
       true
     `);
@@ -21,7 +24,6 @@ function App(): JSX.Element {
     <html>
       <head>
           <script src="https://unpkg.com/@xmtp/xmtp-js@7.14.2/dist/umd/index.js"></script>
-          <script src="./xmtp.js"></script>
       </head>
       <body>
       </body>
@@ -29,20 +31,26 @@ function App(): JSX.Element {
     `;
 
   let jsCode = `
-    var add = function (x) {
-        return function (y) {
-            window.ReactNativeWebView.postMessage(JSON.stringify({
-                key: x+y
-            }), "*");
-            return true; 
-        };
+    function sendGM(account, recipient) {
+      try {
+        var xmtp = await Client.create(account);
+        var conversation = await xmtp.conversations.newConversation(recipient);
+        var message = await conversation.send("gm!");
+        console.log('sent message: ' + message.content);
+        window.ReactNativeWebView.postMessage(JSON.stringify({
+            message: message.content
+        }), "*");
+        return true; 
+      } catch (error) {
+        console.log("error creating client: ".concat(error));
+      }
     }
   `;
 
   return (
     <SafeAreaView>
       <Text style={{marginTop: 100}}>{result}</Text>
-      <Button title="Press me" onPress={onPress} />
+      <Button title="GM from random wallet" onPress={sendGM} />
       <WebView
         ref={webView}
         style={{flex: 1, marginBottom: 20}}
@@ -59,7 +67,7 @@ function App(): JSX.Element {
           console.log('got a message', data);
           const result = JSON.parse(data);
 
-          setResult(result.key);
+          setResult(result.message);
         }}
       />
     </SafeAreaView>
